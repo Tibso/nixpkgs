a34b4ff515523c2ddf54a3d0ed8ce6b081a03698
Tibso <thibaut.dls28@hotmail.com>
2025-07-10 19:25:34 +0200

    beyond-all-reason: add desktop entry

--- pkgs/by-name/be/beyond-all-reason/package.nix
+++ pkgs/by-name/be/beyond-all-reason/package.nix
@@ -5,18 +5,29 @@
   openal,
 }:
 let
-  version = "1.2988.0";
   pname = "beyond-all-reason";
+  version = "1.2988.0";
 
   src = fetchurl {
     url = "https://github.com/beyond-all-reason/BYAR-Chobby/releases/download/v${version}/Beyond-All-Reason-${version}.AppImage";
     hash = "sha256-ZJW5BdxxqyrM2TJTO0SBp4BXt3ILyi77EZx73X8hqJE=";
+  };
+
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
   };
 in
 appimageTools.wrapType2 {
   inherit pname version src;
 
   extraPkgs = pkgs: [ openal ];
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/beyond-all-reason.desktop -t $out/share/applications
+    install -m 444 -D ${appimageContents}/beyond-all-reason.png -t $out/share/icons/hicolor/256x256/apps
+    substituteInPlace $out/share/applications/beyond-all-reason.desktop \
+      --replace-fail 'Exec=AppRun' 'Exec=${pname}'
+  '';
 
   meta = {
     homepage = "https://www.beyondallreason.info/";
@@ -25,8 +36,10 @@ appimageTools.wrapType2 {
     description = "Free Real Time Strategy Game with a grand scale and full physical simulation in a sci-fi setting";
     license = lib.licenses.gpl2Plus;
     platforms = [ "x86_64-linux" ];
+    mainProgram = pname;
     maintainers = with lib.maintainers; [
       kiyotoko
+      tibso
     ];
   };
 }
